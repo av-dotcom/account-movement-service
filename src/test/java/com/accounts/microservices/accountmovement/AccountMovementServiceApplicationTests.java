9c6c8c98bdc479fcd73e4ee7506722db6808bbb1
av-dotcom <me+github@avilaca.com>
2024-08-31 13:23:51 -0400

    first commit

---
+++ src/test/java/com/accounts/microservices/accountmovement/AccountMovementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounts.microservices.accountmovement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMovementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
